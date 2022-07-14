66a6f5a03384944eb164ecd6de75f46bcc293362
MD <dhmedy@googlemail.com>
2022-07-14 15:40:01 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+
+let helloworld = 'Hello World';
+document.writeln(helloworld);
+document.write('<br>');
+const pi = 3.14;
+document.writeln(pi);
+document.write('<br>');
+let burjdubai = '828m';
+document.writeln(burjdubai);
+document.write('<br>');
+fullname = 'Jan Schmidt';
+document.writeln(fullname);
+document.write('<br>');
+let eifeltower = '324m';
+document.writeln(eifeltower);
+document.write('<br>');
+const camelcase = 'camelCase';
+document.writeln(camelcase);
+document.write('<br>');
+let kursstatus = true;
+document.writeln(kursstatus);
